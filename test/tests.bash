4a4c210e4fdc3ed06b98c5e52c4aa693c10b5c89
Benjamin Feng <contact@fengb.me>
2017-04-08 11:55:46 -0500

    More or_names

--- test/tests.bash
+++ test/tests.bash
@@ -6,4 +6,6 @@ __=$'\n'
 
 test_or_names() {
   assert_equals "`or_names <<<foo`" "-name${__}foo"
+  assert_equals "`or_names <<<'foo bar'`" "-name${__}foo bar"
+  assert_equals "`or_names <<<"foo${__}bar"`" "-name${__}foo${__}-or${__}-name${__}bar"
 }
